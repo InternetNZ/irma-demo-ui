aca43828293a47ca6d88cf190f3aa2c3ad0b4ef4
Rafael Bernard Araújo <rafael.araujo@initd.info>
2020-11-05 22:02:22 -0300

    Add age limit and birthdate issuance (#16)
    * Add over18 issuance
    
    * Set ageLimits
    
    * Add birthdate
    
    * Rename internal-affairs to personal-data
    
    * Irma demo over18
    
    * Rename Internal Affairs to Personal Data
    
    * Rename Internal Affairs to Personal Data
    
    * Add simple verifier page

---
+++ global.js
@@ -0,0 +1,18 @@
+
+// ignoring IIFE to test purposes - this should be really globals
+const AWS_IRMA_SERVER = 'http://13.54.7.159:8088';
+const LOCAL_IRMA_SERVER = 'https://2838d2a173e7.ngrok.io';
+
+const IRMA_SERVER = AWS_IRMA_SERVER;
+const AUTH_METHOD_TOKEN = 'token';
+const PRIVATE_TOKEN = 'secret-fake-token';
+
+const CREDENTIAL = {
+  INTERNETNZ_MEMBERSHIP: 'inz-demo.internetnz.membership',
+  FAKE_EMAILER_EMAIL: 'inz-demo.emailer.email',
+  FAKE_PERSONAL_DATA_NAME: 'inz-demo.personal-data.personName',
+  FAKE_PERSONAL_DATA_AGE_LIMITS: 'inz-demo.personal-data.ageLimits',
+  FAKE_PERSONAL_DATA_BIRTHDATE: 'inz-demo.personal-data.birthdate',
+  FAKE_NZPOST_FULL_ADDRESS: 'inz-demo.postal-service.address',
+  IRMA_DEMO_NIJMEGEN_AGE_LIMITS: 'irma-demo.nijmegen.ageLimits',
+};
