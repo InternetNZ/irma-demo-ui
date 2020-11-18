0014aad1b4ca906d983c5ea696b495e4afcd811e
Rafael Bernard Araújo <rafael.araujo@initd.info>
2020-11-17 21:41:59 -0300

    Add ApiGateway URL

--- global.js
+++ global.js
@@ -1,6 +1,7 @@
 
 // ignoring IIFE to test purposes - this should be really globals
-const AWS_IRMA_SERVER = 'http://13.54.7.159:8088';
+//const AWS_IRMA_SERVER = 'http://13.54.7.159:8088';
+const AWS_IRMA_SERVER = 'https://k4erdd8uc6.execute-api.ap-southeast-2.amazonaws.com/dev';
 const LOCAL_IRMA_SERVER = 'https://2838d2a173e7.ngrok.io';
 
 const IRMA_SERVER = AWS_IRMA_SERVER;
@@ -15,4 +16,10 @@ const CREDENTIAL = {
   FAKE_PERSONAL_DATA_BIRTHDATE: 'inz-demo.personal-data.birthdate',
   FAKE_NZPOST_FULL_ADDRESS: 'inz-demo.postal-service.address',
   IRMA_DEMO_NIJMEGEN_AGE_LIMITS: 'irma-demo.nijmegen.ageLimits',
+};
+
+const ATTRIBUTE = {
+  FAKE_EMAILER_EMAIL: CREDENTIAL.FAKE_EMAILER_EMAIL + '.email',
+  FAKE_PERSONAL_DATA_NAME: 'inz-demo.personal-data.personName.fullName',
+  FAKE_NZPOST_FULL_ADDRESS: 'inz-demo.postal-service.address.fullAddress',
 };
