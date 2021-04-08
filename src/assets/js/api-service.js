c8db5f5e794fea05b16de1015a654331ec95e0a8
Rafael Bernard Araújo <rafael.araujo@initd.info>
2021-04-07 23:44:17 -0300

    Improve code style

--- src/assets/js/api-service.js
+++ src/assets/js/api-service.js
@@ -1,31 +1,21 @@
 // ignoring IIFE to test purposes - this should be really globals
 
 const AWS_IRMA_BACKEND_SERVER = 'https://f9emnttxd6.execute-api.ap-southeast-2.amazonaws.com/demo';
-//const AWS_IRMA_BACKEND_SERVER = 'http://localhost:5050';
 
 const SECOND_PRIVATE_TOKEN = 'slU2raoR4f6q1eiVQbYuk5GGI3j2qaL25BdB6SSA';
 
 const checkPassport = (base64Image) => {
-  const body = {
+  return _apiSingleSourcePost('passports', {
     document_image: base64Image,
     country_code: 'NZL',
-  };
-  return _apiSingleSourcePost('passports', body)
-    .then((result) => {
-      return result;
-    });
+  });
 }
 
 const checkDriverLicence = (base64Image) => {
-  const body = {
+  return _apiSingleSourcePost('drivers-licences', {
     document_image: base64Image,
     country_code: 'NZL',
-  };
-  return _apiSingleSourcePost('drivers-licences', body)
-    .then((result) => {
-      console.log(result);
-      return result;
-    });
+  });
 }
 
 const _apiSingleSourcePost = (endpoint, body) => {
