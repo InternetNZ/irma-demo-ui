dd7bc3be45e3013b406fa616286e2786b9a02daf
Rafael Bernard Araújo <rafael.araujo@initd.info>
2021-05-03 16:41:02 -0300

    Add IDNZ attributes to other PoC pages (#40)
    * Apply IDNZ at wine shop
    
    * Add IDNZ card attributes to wine shop and membership
    
    * Cleanup

--- src/assets/js/irma-functions.js
+++ src/assets/js/irma-functions.js
@@ -1,29 +1,29 @@
 // be sure that you are loading irmajs functions
 
 function irmaDiscloseOrSign(attributes, header = 'Disclosing attribute with', label = '', message = '') {
-    const labelRequest = !label ? {} : {
-        'labels': {
-            '0': {
-                'en': label,
-                'nl': label
-            }
-        }
-    };
-    const request = !message ? {
-        '@context': 'https://irma.app/ld/request/disclosure/v2',
-        'disclose': attributes,
-        ...labelRequest
-    } : {
-        '@context': 'https://irma.app/ld/request/signature/v2',
-        'message': message,
-        'disclose': attributes,
-        ...labelRequest
-    };
+  const labelRequest = !label ? {} : {
+    'labels': {
+      '0': {
+        'en': label,
+        'nl': label
+      }
+    }
+  };
+  const request = !message ? {
+    '@context': 'https://irma.app/ld/request/disclosure/v2',
+    'disclose': attributes,
+    ...labelRequest
+  } : {
+    '@context': 'https://irma.app/ld/request/signature/v2',
+    'message': message,
+    'disclose': attributes,
+    ...labelRequest
+  };
 
-    return irmaDoSession(request, header).then(result => {
-        console.log("Successful disclosure! 🎉", result)
-        return result;
-    });
+  return irmaDoSession(request, header).then(result => {
+    console.log("Successful disclosure! 🎉", result)
+    return result;
+  });
 }
 
 function irmaIssueCredential(credential, attributes, header = 'Issuing credential with', disclosePayload = null) {
@@ -49,41 +49,41 @@ function irmaIssueCredential(credential, attributes, header = 'Issuing credentia
 }
 
 function irmaDoSession(request, header = '') {
-    console.log('doSession');
+  console.log('doSession');
 
-    let options = {
-        // Developer options
-        debugging: true,
+  let options = {
+    // Developer options
+    debugging: true,
 
-        // Front-end options
-        language: 'en',
-        translations: {
-            header: `${header} <i class="irma-web-logo">IRMA</i>`,
-            loading: 'Just one second please!'
+    // Front-end options
+    language: 'en',
+    translations: {
+      header: `${header} <i class="irma-web-logo">IRMA</i>`,
+      loading: 'Just one second please!'
+    },
+
+    // Back-end options
+    session: {
+      // Point this to your IRMA server:
+      url: IRMA_SERVER,
+
+      // Define your disclosure request:
+      start: {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': PRIVATE_TOKEN,
         },
+        body: JSON.stringify(request)
+      }
+    }
+  };
 
-        // Back-end options
-        session: {
-            // Point this to your IRMA server:
-            url: IRMA_SERVER,
+  console.log(options);
 
-            // Define your disclosure request:
-            start: {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': PRIVATE_TOKEN,
-                },
-                body: JSON.stringify(request)
-            }
-        }
-    };
+  let irmaPopup = irma.newPopup(options);
 
-    console.log(options);
-
-    let irmaPopup = irma.newPopup(options);
-
-    return irmaPopup.start()
+  return irmaPopup.start()
 }
 
 const irmaDisclosedResultSingleRawValueFromIndex = function (result, credentialIndex = 0, attributeIndex = 0) {
@@ -96,6 +96,11 @@ const irmaDisclosedResultSingleRawValue = function (result) {
 
 const DiscloseQueryGenerator = function () {
   this.elements = [];
+
+  const _init = function () {
+    this.elements = [];
+    return this;
+  }
 
   const andAttribute = function (attribute) {
     this.elements.push([
@@ -114,9 +119,24 @@ const DiscloseQueryGenerator = function () {
     return this.andAttribute(attribute);
   }
 
+  const orAttributes = function (...attributes) {
+    const or = [];
+    attributes.forEach(attribute => {
+      if (Array.isArray(attribute)) {
+        or.push(attribute);
+      } else {
+        or.push([attribute]);
+      }
+    });
+    this.elements.push(or);
+    return this;
+  }
+
   return {
-    forAttribute: forAttribute,
+    _init: _init,
     andAttribute: andAttribute,
+    forAttribute: forAttribute,
+    orAttributes: orAttributes,
     toApi: toApi,
   };
 }
