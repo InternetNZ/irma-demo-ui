9bceae5c1feafda4db4493e45a9eb20b98a15c49
asher <asher@internetnz.net.nz>
2021-10-05 15:30:59 +1300

    Add static data to replace single source backend

--- src/assets/js/api-service.js
+++ src/assets/js/api-service.js
@@ -5,17 +5,36 @@ const AWS_IRMA_BACKEND_SERVER = 'https://f9emnttxd6.execute-api.ap-southeast-2.a
 const SECOND_PRIVATE_TOKEN = 'slU2raoR4f6q1eiVQbYuk5GGI3j2qaL25BdB6SSA';
 
 const checkPassport = (base64Image) => {
-  return _apiSingleSourcePost('passports', {
-    document_image: base64Image,
-    country_code: 'NZL',
-  });
+ return {
+          "givenNames": "Sam",
+          "familyName": "Specimen",
+          "gender": "F",
+          "documentNumber": "PA1234567",
+          "nationality": "NZL",
+          "issuerCountry": "NZL",
+          "birthDate": "1990-04-18",
+          "dateOfExpiry": "2025-04-18",
+          "governmentVerified": "verified",
+          "verificationReference": "3d7f9ed3-8bf7-41e8-868b-7cc679a0397f"
+        });
 }
 
 const checkDriverLicence = (base64Image) => {
-  return _apiSingleSourcePost('drivers-licences', {
-    document_image: base64Image,
-    country_code: 'NZL',
-  });
+  return {
+           "givenNames": "John",
+           "familyName": "Smith",
+           "documentNumber": "DT1234565",
+           "birthDate": "1984-04-06",
+           "dateOfExpiry": "",
+           "governmentVerified": {
+               "driverLicence": True,
+               "familyName": True,
+               "givenName": True,
+               "birthDate": True
+           },
+           "verificationReference": "a5f433c58-f516-4352-845f-055534f6698c",
+           "documentVersion": "980"
+         });
 }
 
 const _apiSingleSourcePost = (endpoint, body) => {
---
+++ src/assets/js/single-source-api-service.js
@@ -0,0 +1,34 @@
+// ignoring IIFE to test purposes - this should be really globals
+
+const AWS_IRMA_BACKEND_SERVER = 'https://f9emnttxd6.execute-api.ap-southeast-2.amazonaws.com/demo';
+
+const SECOND_PRIVATE_TOKEN = 'slU2raoR4f6q1eiVQbYuk5GGI3j2qaL25BdB6SSA';
+
+const checkPassport = (base64Image) => {
+  return _apiSingleSourcePost('passports', {
+    document_image: base64Image,
+    country_code: 'NZL',
+  });
+}
+
+const checkDriverLicence = (base64Image) => {
+  return _apiSingleSourcePost('drivers-licences', {
+    document_image: base64Image,
+    country_code: 'NZL',
+  });
+}
+
+const _apiSingleSourcePost = (endpoint, body) => {
+  const url = `${AWS_IRMA_BACKEND_SERVER}/single-source/${endpoint}`;
+  const options = {
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': SECOND_PRIVATE_TOKEN,
+    },
+    mode: 'cors',
+  };
+
+  return fetch(url, {method: 'POST', ...options})
+    .then(response => response.json());
+};
