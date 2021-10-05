d7290da94dbc8f95d21d8f690fad8205cd41d155
asher <asher@internetnz.net.nz>
2021-10-06 12:07:00 +1300

    Use logged in users name for fake passport and drivers licence data

--- src/assets/js/api-service.js
+++ src/assets/js/api-service.js
@@ -4,25 +4,27 @@ const AWS_IRMA_BACKEND_SERVER = 'https://f9emnttxd6.execute-api.ap-southeast-2.a
 
 const SECOND_PRIVATE_TOKEN = 'slU2raoR4f6q1eiVQbYuk5GGI3j2qaL25BdB6SSA';
 
-const checkPassport = (base64Image) => {
- return {
-          "givenNames": "Sam",
-          "familyName": "Specimen",
-          "gender": "F",
-          "documentNumber": "PA1234567",
-          "nationality": "NZL",
-          "issuerCountry": "NZL",
-          "birthDate": "1990-04-18",
-          "dateOfExpiry": "2025-04-18",
-          "governmentVerified": "verified",
-          "verificationReference": "3d7f9ed3-8bf7-41e8-868b-7cc679a0397f"
-        });
+const checkPassport = (base64Image, nameArray) => {
+  familyName = nameArray.pop();
+  return {
+           "givenNames": nameArray.join(' ');,
+           "familyName": familyName,
+           "gender": "F",
+           "documentNumber": "PA1234567",
+           "nationality": "NZL",
+           "issuerCountry": "NZL",
+           "birthDate": "1990-04-18",
+           "dateOfExpiry": "2025-04-18",
+           "governmentVerified": "verified",
+           "verificationReference": "3d7f9ed3-8bf7-41e8-868b-7cc679a0397f"
+         });
 }
 
-const checkDriverLicence = (base64Image) => {
+const checkDriverLicence = (base64Image, nameArray) => {
+  familyName = nameArray.pop();
   return {
-           "givenNames": "John",
-           "familyName": "Smith",
+           "givenNames": nameArray.join(' ');,
+           "familyName": familyName,
            "documentNumber": "DT1234565",
            "birthDate": "1984-04-06",
            "dateOfExpiry": "",
